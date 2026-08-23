64fb1042484fb39bb3586af84b65d7cba71c0514
joshuatec7-prog <joshuatec7@gmail.com>
2026-08-23 16:40:56 +0200

    Install Pushover hooks in auto live runner

--- diamond_auto_live_5_runner.py
+++ diamond_auto_live_5_runner.py
@@ -9,9 +9,11 @@ os.environ["DIAMOND_AUTO_LIVE_5_ENABLED"] = "1"
 
 from diamond_auto_live_5_patch import install_auto_live_5_patch
 from diamond_auto_live_5_guard import install_auto_live_5_guard
+from diamond_pushover_alerts import install_pushover_hooks
 
 install_auto_live_5_patch()
 install_auto_live_5_guard()
+install_pushover_hooks()
 
 from closed_candle_runner import run_bot
 
